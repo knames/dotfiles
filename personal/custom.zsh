dbe2e3da88f7f1f0a5aafe88871434b420e5e582
ken slawinski <ken@kbp14.local>
2022-03-25 12:53:17 -0600

    git shortcuts

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,7 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
--- personal/spin.zsh
+++ personal/spin.zsh
@@ -1,2 +1,6 @@
 # Custom configs for Spin environments
 # This file will only be executed on Spin environments.
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
